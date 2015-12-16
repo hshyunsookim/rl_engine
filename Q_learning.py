cb784f3ebbf7c26a4c5ffe77c6c1b8a4e88ff4f1
hsk_laptop_ubuntu <hskim@cs.duke.edu>
2015-12-17 01:30:56 +0900

    more updates

--- Q_learning.py
+++ Q_learning.py
@@ -2,6 +2,7 @@
 
 import taxi_domain as dm
 import random as r
+import collections
 
 
 # Initialize game state
@@ -10,67 +11,131 @@ s = dm.gameState()
 s.setStates(xt,yt,xp,yp,pickup, xg,yg)
 
 # Set parameters and variables
-alpha = 0.1   # Learning rate
+alpha = 0.5   # Learning rate
 gamma = 0.9   # Discount factor
-epsilon = 0.1 # Exploration factor (exploration vs. exploitation)
-lamda = 0.1   # Eligibility trace decay rate 
+epsilon = 0.25 # Exploration factor (exploration vs. exploitation)
+lamda = 0.5   # Eligibility trace decay rate 
 			  # (didn't want to overwrite python built-in name "lambda" here)
+
 
 numMoves = 0  # Count number of moves in an episode
 
-## Time = t0
-# Store current state (state at time t0)
-s_t0 = s.getCurrState()
+# Q-value Greedy Policy 
+def evaluatePolicy(state):
+	for s in state.Q.keys():
+		Q_values = state.Q.get(s)
 
-# Get best action for current state
-if r.random() < epsilon:
-	a_t0 = s.getAction()
-else:
-	a_t0 = r.randint(1,6)
+		# for each state, choose argmax_a Q(s,a) 
+		maxVal = max(Q_values)
+		maxIndices = findMaxInd(maxVal, Q_values)
+		maxIdx = r.randint(0,len(maxIndices)-1)
+		greedy_action = maxIndices[maxIdx]		
+		state.pi[(s)] = greedy_action
 
-# Table look-up for Q-value at (s,a) 
-Qval_curr = s.getValue(a_t0)
 
-while not s.checkEoE():
-	# Change current state using chosen action, and receive a reward
-	reward = s.changeState(a_t0)
+def findMaxInd(val, list):
+	idx = -1
+	indices = []
+	while True:
+		try:
+			idx = list.index(val, idx+1)
+			indices.append(idx)
+		except ValueError:
+			break
+	return indices
 
-	## Time = t1
-	# Store new state (state at time t1)
-	s_t1 = s.getCurrState()
+# Functions for debugging
+def printEtrace():
+	for v in s.e.values():
+		for i in  range(len(v)):
+			if v[i] != 0.0:
+				print v[i]
 
-	# Get best action for next state
-	if r.random() < epsilon:
-		a_t1 = s.getAction()
+def printUniqueQ():
+	for v in s.Q.values():
+		for i in range(len(v)):
+			if v[i] != 0:
+				print v[i]
+	raw_input(" ")	
+
+def printOrderedQ():
+	orderedQ = collections.OrderedDict(sorted(s.Q.items()))
+	for state, value in orderedQ.iteritems():
+		print state, value
+	raw_input(" ")
+
+
+for i in range(51):
+	numMoves = 0
+	(xt,yt,xp,yp,pickup), (xg,yg) = [[0,0,0,4,0],[4,4]]
+	s.setStates(xt,yt,xp,yp,pickup, xg,yg)
+	s.clearTraces()	
+	# retain Q-function from previous episode	
+	# printOrderedQ()
+
+	## Time = t0
+	# Store current state (state at time t0)
+	s_t0 = s.getCurrState()
+
+	# Get best action for current state
+	if r.random() > epsilon:
+		a_t0 = s.getAction()
 	else:
-		a_t1 = r.randint(1,6)
+		a_t0 = r.randint(1,6)
 
-	# Table look-up for Q-value at (s',a')
-	Qval_next = s.getValue(a_t1)
-
-	## Update Q-value
-	# TD-error
-	delta = reward + gamma*Qval_next - Qval_curr
-
-	# Eligibility Trace 
-	''' Need to Implement Here '''
-	e = 1
-
-	# Update Q
-	s.setValue(s_t0, a_t0, alpha*delta)
-
-	## Take a time-step
-	s_t0 = s_t1
-	a_t0 = a_t1
-	Qval_curr = Qval_next
-
-	# Print progress
-	numMoves += 1
-	print "Move #", numMoves, "\n"
-
-print "End of Episode"
+	# Table look-up for Q-value at (s,a) 
+	Qval_curr = s.getValue(a_t0)
 
 
-# To Do:
-# implement e-traces
-# update policy by max-ing Q(s,a) after each Q-value update
+	while not s.checkEoE():
+		# Eligibility Trace 
+		s.updateEtrace(a_t0, lamda)
+		# printEtrace()
+
+		# Change current state using chosen action, and receive a reward
+		reward = s.changeState(a_t0)
+
+		## Time = t1
+		# Store new state (state at time t1)
+		s_t1 = s.getCurrState()
+
+		# Get best action for next state
+		if r.random() > epsilon:
+			a_t1 = s.getAction()
+		else:
+			a_t1 = r.randint(1,6)
+
+		# Table look-up for Q-value at (s',a')
+		if s.EoE:
+			Qval_next = 0 
+		else:
+			Qval_next = s.getValue(a_t1)
+
+		## Update Q-value
+		# TD-error
+		delta = reward + gamma*Qval_next - Qval_curr
+
+
+		# Update Q
+		for key in s.Q.keys():
+			values = s.e.get((key))
+			for actions in range(len(values)):
+				newVal = s.Q.get((key))[actions] + values[actions-1]*alpha*delta
+				s.setValue(key, actions, newVal)
+		
+
+		## Take a time-step
+		s_t0 = s_t1
+		a_t0 = a_t1
+		Qval_curr = Qval_next
+
+		# Update the policy for the new Q-function 
+		evaluatePolicy(s)
+
+		# Print progress
+		numMoves += 1
+		if numMoves%1000 == 0:
+			print "."
+		#print "Eps #", i, "  Move #", numMoves, "  State: ", s_t0, "  Action: ", a_t0
+
+	print "End of Episode: ", i, " in ", numMoves, " moves"
--- taxi_domain.py
+++ taxi_domain.py
@@ -15,7 +15,7 @@ taxiMap = [[(2,3,5,6),   (3,4,5,6),     (2,3,5,6),     (2,3,4,5,6),   (3,4,5,6)]
            [(1,3,5,6),   (1,2,3,5,6),   (1,3,4,5,6),   (1,2,3,5,6),   (1,3,4,5,6)], 
            [(1,5,6),     (1,2,5,6),     (1,4,5,6),     (1,2,5,6),     (1,4,5,6) ]]
 
-# Initialize tabular Q-function, set values for all 625 states to zero
+# 1) Initialize tabular Q-function, set values for all 625 states to zero
 # Structured as a map, where key = (xt,yt,xp,yp,pickup) and value = 6-element tuple, 
 # holding one value for each action.  
 # Ex.) Q.get( (xt,yt,xp,yp,pickup) )[action]
@@ -23,21 +23,26 @@ taxiMap = [[(2,3,5,6),   (3,4,5,6),     (2,3,5,6),     (2,3,4,5,6),   (3,4,5,6)]
 #      when the taxi is at position (1,1), passenger is at (5,5) and the passenger 
 # 	   is not picked up (pick-up = 0) 
 
-# In addition, initialize tabular Policy (Pi)
+# 2) Initialize tabular Policy (Pi)
 # Before any learning, the policy is initialized as random action for every state
 # because the value of each state-action pair is all zero. (random tie-breaks)
 
-Q, pi = {}, {}
+# 3) Initialize eligibility traces (to be optionally used)
+# e-trace keeps track of a list of past states, and a weight that decays with time
+# Most recently visited state receives a weight of 1 (full update), whereas
+# weight of past states are scaled by a factor (lambda) at each time step
+Q, pi, e = {}, {}, {}
 for xt in range(5):
 	for yt in range(5):
 		for xp in range(5):
 			for yp in range(5):
 				Q[(xt,yt,xp,yp,0)] = (0,0,0,0,0,0)
 				pi[(xt,yt,xp,yp,0)] = randint(1,6)
+				e[(xt,yt,xp,yp,0)] = (0,0,0,0,0,0)
 		# When passenger is picked-up, (x,y) positions of taxi and passenger are the same
 		Q[(xt,yt,xt,yt,1)] = (0,0,0,0,0,0)
 		pi[(xt,yt,xt,yt,1)] = randint(1,6)
-
+		e[(xt,yt,xt,yt,1)] = (0,0,0,0,0,0)
 
 # GameState Class:
 # start state: (x,y) positions of taxi, passenger, and pick-up state
@@ -61,6 +66,9 @@ class gameState:
 
 		# Policy
 		self.pi = pi
+
+		# Eligibility traces
+		self.e = e
 
 		# Domain map
 		self.map = taxiMap
@@ -89,13 +97,15 @@ class gameState:
 
 	def getValue(self, action):
 		state = (self.xt, self.yt, self.xp, self.yp, self.pickup)
-		print "State: ", state, " Action: ", action
+		# print "State: ", state, " Action: ", action
 		return self.Q.get((state))[action-1] # actions: 1~6, index: 0~5
 
 	def setValue(self, state, action, newValue):
+		curr = self.Q.get((state))
 		values = list(self.Q.get((state)))
 		values[action-1] = newValue
 		self.Q[(state)] = tuple(values)
+		# print "s:", state, "curr: ", curr, " newVal: ", newValue, "  new: ", self.Q.get((state))
 
 	def getAction(self):
 		state = (self.xt, self.yt, self.xp, self.yp, self.pickup)
@@ -133,6 +143,7 @@ class gameState:
 			if action == 5:
 				if self.checkSamePosition():
 					self.pickup = 1
+					
 				else:
 					# attempted pick-up, failed because taxi and passenger are
 					# not at the same location. Penalize with -10 reward
@@ -140,6 +151,11 @@ class gameState:
 
 			# Drop off
 			elif action == 6:
+				# attempting to drop-off when passenger is not on the taxi
+				# is penalized with -10 reward
+				if not self.pickup:
+					r = -10 
+
 				self.pickup = 0
 
 				# End-of-Episode gets +20 reward
@@ -156,14 +172,22 @@ class gameState:
 	def checkSamePosition(self):
 		return (self.xt==self.xp) & (self.yt==self.yp)
 
+	def updateEtrace(self, action, lamda):
+		for key in e.keys():
+			val = list(self.e[(key)])
+			for i in range(len(val)):
+				val[i] *= lamda
+			e[(key)] = tuple(val)
+
+		state = (self.xt, self.yt, self.xp, self.yp, self.pickup)
+		val = list(self.e.get((state)))
+		val[action-1] = 1
+		self.e[(state)] = tuple(val)
+
+	def clearTraces(self):
+		for key in self.e.keys():
+			self.e[(key)] = (0,0,0,0,0,0);
+
 	def checkEoE(self):
 		return self.checkSamePosition() & (self.xg==self.xp) & \
 			    (self.yg==self.yp) & (self.pickup == 0)
-
-'''
-s = gameState()
-s.printState()
-s.getValue( (1,1,2,2,0), 0 )
-print s.getCurrState()
-print s.getGoalState()'''
-				
